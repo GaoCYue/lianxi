833b579c706d6532717773760756d069fb82b10d
gao <1536576466@qq.com>
2020-07-16 17:26:46 +0800

    添加checkbox

---
+++ src/pages/checkbox.vue
@@ -0,0 +1,30 @@
+<route>
+{
+  "name": "checkbox",
+  "meta": {
+    "linkNmae": "checkbox"
+  }
+}
+</route>
+<template>
+  <div>
+        <input type="checkbox" id="checkbox1" value="value1" v-model="value1">
+        <label for="checkbox">{{ value1 }}</label>
+        <input type="checkbox" value="value2" id="checkbox2" v-model="value1">
+        <label for="checkbox">{{ value2 }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+      return {
+          value1: [],
+          value2: true
+      }
+  }
+};
+</script>
+
+<style>
+</style>
